066d02b951d67972050775007faba6189ed61174
Timothy Cherney <tjcherney@hotmail.com>
2026-08-06 02:22:20 -0700

    forcing portrait for now

--- app/src/main/java/com/tcherney/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/tcherney/barcodescanner/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tcherney.barcodescanner
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -42,6 +43,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
             Home()
         }
     }
